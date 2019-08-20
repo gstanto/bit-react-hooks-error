d13d424b00630fa20d4724981b35c5eb40fe6b52
Gary Stanton <gary.stanton@treehousetechgroup.com>
2019-08-20 02:35:51 -0400

    moved to open repo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Square from '@bit/gstanto.open.square-2';
-import UserMenu from '@bit/gstanto.treehouse_tech.user-menu';
+import UserMenu from '@bit/gstanto.open.user-menu';
 
 function App() {
   return (
